a78c247346dbba85d8d60d982d9ea6aba8fad03d
ooakforhome <ddmbaatlanta@gmail.com>
2018-10-04 09:16:55 -0400

    veg updates

--- config/config.example.js
+++ config/config.example.js
@@ -1,15 +1,4 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-
-
 module.exports = {
-  db: 'mongodb://localhost:27017/Vegreen',
-  db_dev: 'mongodb://localhost:27017/Vegreen',
+  db: 'mongodb://vegreen:12345@localhost:27017/Vegreen',
+  db_dev: 'mongodb://vegreen:12345@localhost:27017/Vegreen',
 };
-
-
-
-// module.exports = {
-//   db: 'mongodb://david:12345@localhost:27017/my_db',
-//   db_dev: 'mongodb://david:12345@localhost:27017/my_db'
-// };
--- server/routes/api/contacts.js
+++ server/routes/api/contacts.js
@@ -1,9 +1,17 @@
+
 const Contact = require('../../models/Contact');
 
 module.exports = (app) => {
   app.post('/api/newcontact', (req, res, next) => {
     Contact.create(req.body)
       .then(contact => res.json(contact))
+      .catch((err) => next(err));
+  });
+
+  app.get('/api/allcontact', (req, res, next) => {
+    Contact.find()
+      .exec()
+      .then((contact) => res.json(contact))
       .catch((err) => next(err));
   });
 };
